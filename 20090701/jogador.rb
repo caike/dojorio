b63be822572431c3af39f3758a8220864ac449b7
caike souza <carloshrsouza@gmail.com>
2009-07-14 17:25:40 -0300

    refatorada e passando

--- 20090701/jogador.rb
+++ 20090701/jogador.rb
@@ -1,33 +1,46 @@
-class JogadorExpulsoException < StandardError
-    
-end
+class JogadorExpulsoException < StandardError; end
 
 class Jogador
-    attr_accessor :cartoes
-    
-    def initialize
-        @cartoes = []        
-    end
+  
+  attr_accessor :cartoes
 
-    def cometer_falta (jogador, options = {})
-        raise JogadorExpulsoException if expulso?
-        cartao = cartao_cor (options[:tipo])
-        @cartoes << cartao unless cartao.nil?
+  def initialize
+    @cartoes = Array.new        
+  end
+
+  def cometer_falta(jogador, options = { :tipo => :leve })
+    raise JogadorExpulsoException if expulso?
+    cartao = cartao_cor(options[:tipo])
+    @cartoes.concat(cartao) if cartao
+  end
+
+  def expulso?
+    @cartoes.include?('vermelho')
+  end
+  
+  protected
+  
+  def cartao_cor(tipo)
+    case tipo
+    when :grave then penalidade_grave
+    when :gravissima then penalidade_gravissima                   
     end
-    
-    def cartao_cor (tipo)
-        if (tipo == :grave)
-            if @cartoes == ['amarelo']
-                return ['amarelo', 'vermelho']
-            end
-            return 'amarelo'
-            
-        elsif (tipo == :gravissima)
-            return 'vermelho'            
-        end
+  end
+                              
+  def penalidade_grave        
+    novos_cartoes = ['amarelo']
+    if ja_penalizado?
+      novos_cartoes.concat(['vermelho']) 
     end
-    
-    def expulso?
-         @cartoes.include?('vermelho')
-    end
+    novos_cartoes
+  end  
+  
+  def penalidade_gravissima
+    ['vermelho'] 
+  end
+  
+  def ja_penalizado?
+    @cartoes == ['amarelo']
+  end
+  
 end
